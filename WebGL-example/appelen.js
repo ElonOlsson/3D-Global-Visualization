f5c9339525d9283292cabee533db5b9a123a1231
elinlager <elinjohannalager@gmail.com>
2016-05-04 17:29:29 +0200

    laddat in objektet

--- WebGL-example/appelen.js
+++ WebGL-example/appelen.js
@@ -1,5 +1,6 @@
 
 
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(60,window.innerWidth / window.innerHeight, 0.1,10000);
 var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.6 );
@@ -9,17 +10,15 @@ scene.add(new THREE.AmbientLight(0x404040));
 
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window. innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 //Jorden
-var sphere = new THREE.SphereGeometry(0.5, 32, 32);
+/*var sphere = new THREE.SphereGeometry(0.5, 32, 32);
 var material = new THREE.MeshPhongMaterial();
-
-
 var earthMesh = new THREE.Mesh( sphere, material);
 
-scene.add(earthMesh);
+scene.add(earthMesh);*/
 
 //Controll
 var controls = new THREE.OrbitControls(camera,renderer.domElement);
@@ -37,21 +36,24 @@ var manager = new THREE.LoadingManager();
  console.log(item, loaded, total);
  };
 
+
  var loader = new THREE.OBJLoader(manager);
  loader.load('objecWorldMapTest.obj', function (object) {
-
+    console.log(object);
      scene.add(object);
+
  } );
 
 
 
-function render()
-{
-    requestAnimationFrame(render);
+  function render()
+  {
+  requestAnimationFrame(render);
+  controls.update();
+  renderer.render(scene, camera);
+  }
+  render();
 
-    controls.update();
-    renderer.render(scene, camera);
-}
-render();
+
 
 
