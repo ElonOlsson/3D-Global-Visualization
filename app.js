8f6c8b2181f613cd23ceb2f5170b42d1a4cbb4df
erikrudhe <erik.rudhe@hotmail.com>
2016-05-10 17:07:16 +0200

    gfgfgf

---
+++ app.js
@@ -0,0 +1,94 @@
+/**
+ * Created by Erik on 2016-05-04.
+ */
+
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(60,window.innerWidth / window.innerHeight, 0.1,10000);
+var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.6 );
+directionalLight.position.set( 20, 0, 20 );
+scene.add( directionalLight );
+scene.add(new THREE.AmbientLight(0x404040));
+
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window. innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+//Jorden
+var sphere = new THREE.SphereGeometry(0.5, 32, 32);
+var material = new THREE.MeshPhongMaterial();
+material.shininess = 8;
+material.map = THREE.ImageUtils.loadTexture('earthmap1k.jpg');
+material.bumpMap = THREE.ImageUtils.loadTexture('earthbump1k.jpg');
+material.bumpScale =  0.08;
+material.specularMap = THREE.ImageUtils.loadTexture('earthspec1k.jpg');
+material.specular = new THREE.Color('grey');
+
+var earthMesh = new THREE.Mesh( sphere, material);
+
+scene.add(earthMesh);
+
+//Clouden
+var geometryCloud = new THREE.SphereGeometry(0.51, 32, 32);
+var materialCloud = new THREE.MeshPhongMaterial();
+
+//materialCloud.map = new THREE.ImageUtils.loadTexture('clouds.png');
+materialCloud.map = new THREE.ImageUtils.loadTexture('earthcloudmaptrans.jpg')
+materialCloud.side = THREE.DoubleSide;
+materialCloud.opacity = 0.2;
+materialCloud.transparent = true;
+materialCloud.depthWrite = false;
+
+console.log("Easter egg. //LiTHehack was here");
+var cloudMesh = new THREE.Mesh(geometryCloud,materialCloud);
+earthMesh.add(cloudMesh);
+
+//Galaxerna
+var geometryStars = new THREE.SphereGeometry(100, 32, 32);
+var materialStars = new THREE.MeshBasicMaterial();
+
+materialStars.map = THREE.ImageUtils.loadTexture('abc123.jpg');
+materialStars.side = THREE.BackSide;
+
+var starMesh = new THREE.Mesh(geometryStars, materialStars);
+scene.add(starMesh);
+
+var controls = new THREE.OrbitControls(camera,renderer.domElement);
+//controls.addEventListener('change', render);
+controls.enableDamping = true;
+controls.dampingFactor = 0.9;
+controls.enableZoom = true;
+
+camera.position.z = 4;
+
+var manager = new THREE.LoadingManager();
+manager.onProgress = function ( item,loaded,total)
+{
+    console.log(item,loaded,total);
+};
+
+//var loader = new THREE.OBJLoader(manager);
+//loader.load('objecWorldMapTest.obj',function (object) {
+//	object.traverse(function (child) {
+////			child.material.map = texture;
+//		}
+//	}
+//}
+//scene.add(object);
+
+
+function render()
+{
+    requestAnimationFrame(render);
+
+    earthMesh.rotation.y += 0.003;
+    cloudMesh.rotation.y += 0.004;
+    starMesh.rotation.x += 0.0002;
+
+    controls.update();
+    renderer.render(scene, camera);
+}
+render();
+
+
---
+++ appelen.js
@@ -0,0 +1,115 @@
+
+
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(60,window.innerWidth / window.innerHeight, 0.1,10000);
+var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.6 );
+directionalLight.position.set( 20, 0, 20 );
+scene.add( directionalLight );
+scene.add(new THREE.AmbientLight(0x404040));
+
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+//Jorden
+var sphere = new THREE.SphereGeometry(0.5, 32, 32);
+var material = new THREE.MeshPhongMaterial();
+var earthMesh = new THREE.Mesh( sphere, material);
+
+scene.add(earthMesh);
+
+//Controll
+var controls = new THREE.OrbitControls(camera,renderer.domElement);
+//controls.addEventListener('change', render);
+controls.enableDamping = true;
+controls.dampingFactor = 0.9;
+controls.enableZoom = true;
+
+camera.position.z = 4;
+
+// read object
+var manager = new THREE.LoadingManager();
+ manager.onProgress = function (item, loaded, total)
+ {
+ console.log(item, loaded, total);
+ };
+
+
+
+ var loader = new THREE.OBJLoader(manager);
+ loader.load('objecWorldMapTest.obj', function (object) {
+
+     console.log(object);
+     scene.add(object);
+
+
+     //ELIN TESTAR
+     //var asia = object.getObjectByName("Asien");
+     //asia.add(object.getObjectByName("Asien2"));
+
+     //var obj = new THREE.Object3D();
+    // Put all segments of the model together into a single object
+     /*for (c = 0; c < 14; c++) {
+         obj.add(object.children[c]);
+     }
+     obj.scale.set(1,5,1);
+
+      intersects[0].object.children[15].material.color.setHex( "111111" );*/
+
+
+
+     //Asien
+     var asia=(object.children[0]);
+
+     for (i=1; i<13; i++) {
+         object.children[i].scale.set(1,1,1);
+
+     }
+
+
+     //Oceanien
+     for (i=15; i<18; i++) {
+         object.children[i].scale.set(1,1,1);
+
+     }
+
+     //Afrika
+     for (i=18; i<20; i++) {
+         object.children[i].scale.set(1,1,1);
+
+     }
+
+     //Nordamerika
+     for (i=20; i<21; i++) {
+         object.children[i].scale.set(1,1,1);
+
+     }
+
+     //Sydamerika
+     for (i=21; i<22; i++) {
+         object.children[i].scale.set(1,1,1);
+
+     }
+
+     //Europa
+     for (i=22; i<27; i++) {
+         object.children[i].scale.set(1,1,1);
+
+     }
+
+ } );
+
+
+  function render()
+  {
+  requestAnimationFrame(render);
+  controls.update();
+  renderer.render(scene, camera);
+  }
+  render();
+
+
+
+
