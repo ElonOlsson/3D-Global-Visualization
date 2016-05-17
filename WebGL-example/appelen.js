640d4b2d3c4053b8ef233bf974fb0d34f610bf37
josefineflygge <josefineflygge@hotmail.com>
2016-05-17 14:49:45 +0200

    josses ändring

--- WebGL-example/appelen.js
+++ WebGL-example/appelen.js
@@ -23,13 +23,20 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 /****************************************
-            SKAPAR GLOBEN
+ SKAPAR GLOBEN
  ***************************************/
 var sphere = new THREE.SphereGeometry(10.5, 32, 32);
 var material = new THREE.MeshPhongMaterial({ color: 0x156289} );
 var earthMesh = new THREE.Mesh( sphere, material);
-
 scene.add(earthMesh);
+
+var geometryStars = new THREE.SphereGeometry(50, 32, 32);
+var materialStars = new THREE.MeshBasicMaterial();
+materialStars.map = THREE.ImageUtils.loadTexture('stars.png');
+materialStars.side = THREE.BackSide;
+var starMesh = new THREE.Mesh(geometryStars, materialStars);
+scene.add(starMesh);
+
 
 //Controll
 var controls = new THREE.OrbitControls(camera,renderer.domElement);
@@ -39,13 +46,13 @@ controls.dampingFactor = 0.9;
 controls.enableZoom = true;
 
 //Zoomar man ut mer så missar ljuset globen
-controls.minDistance = 20;
-controls.maxDistance = 82;
+controls.minDistance = 30;
+controls.maxDistance = 60;
 
 
 
 /****************************************
-            LÄS IN KARTAN
+ LÄS IN KARTAN
  ***************************************/
 var manager = new THREE.LoadingManager();
 manager.onProgress = function (item, loaded, total)
@@ -65,60 +72,81 @@ loader.load('thisistheultimatemap.obj', function (object) {
     var Asia = object.getObjectByName("Asien");
     var positionAsia = Asia.geometry.attributes.position.array;
     for(i=0; i<=positionAsia.length; i +=3) {
-        positionAsia[i+2] *= 10;
+        positionAsia[i+2] *= 12;
     }
 
     var Oceanien = object.getObjectByName("Oceanien");
     var positionOceanien = Oceanien.geometry.attributes.position.array;
     for(i=0; i<=positionOceanien.length; i +=3) {
-        positionOceanien[i+2] *= 10;
+        positionOceanien[i+2] *= 1;
     }
 
     var Europe = object.getObjectByName("Europa");
     var positionEurope= Europe.geometry.attributes.position.array;
     for(i=0; i<=positionEurope.length; i +=3) {
-        positionEurope[i+2] *= 10;
+        positionEurope[i+2] *= 2;
     }
 
     var Northamerica = object.getObjectByName("Nordamerika");
     var positionNorthamerica = Northamerica.geometry.attributes.position.array;
     for(i=0; i<=positionNorthamerica.length; i +=3) {
-        positionNorthamerica[i+2] *= 10;
+        positionNorthamerica[i+2] *= 1;
     }
+
 
     var Southamerica = object.getObjectByName("Sydamerika");
     var positionSouthamerica = Southamerica.geometry.attributes.position.array;
     for(i=0; i<=positionSouthamerica.length; i +=3) {
-        positionSouthamerica[i+2] *= 10;
+        positionSouthamerica[i+2] *= 1.8;
     }
 
     var Africa = object.getObjectByName("Afrika");
     var positionAfrica = Africa.geometry.attributes.position.array;
     for(i=0; i<=positionAfrica.length; i +=3) {
-        positionAfrica[i+2] *= 10;
+        positionAfrica[i+2] *= 3.2;
     }
 
 
+    scaleColor(positionAsia[i+2], Asia);
+    scaleColor(positionOceanien[i+2], Oceanien);
+    scaleColor(positionEurope[i+2], Europe);
+    scaleColor(positionNorthamerica[i+2], Northamerica);
+    scaleColor(positionSouthamerica[i+2], Southamerica);
+    scaleColor(positionAfrica[i+2], Africa);
+
+
+    /*****************************************************
+     Funktion för att färga kontinenter beroende av höjd
+     ****************************************************/
+
+    function scaleColor(continentPosition, continentObj) {
+
+        if (continentPosition >= 3) {
+            continentObj.material.color.setHex(0x156249)
+        }
+
+    }
+
     /*var asia = object.getObjectByName("Asien");
-    asia.scale.set(1,1,1);
+     asia.scale.set(1,1,1);
 
-    var europe = object.getObjectByName("Europa");
-    europe.scale.set(1,1,1);
+     var europe = object.getObjectByName("Europa");
+     europe.scale.set(1,1,1);
 
-    var oceanien = object.getObjectByName("Oceanien");
-    oceanien.scale.set(1,1,1);
+     var oceanien = object.getObjectByName("Oceanien");
+     oceanien.scale.set(1,1,1);
 
-    var northamerica = object.getObjectByName("Nordamerika");
-    northamerica.scale.set(1,1,1);
+     var northamerica = object.getObjectByName("Nordamerika");
+     northamerica.scale.set(1,1,1);
 
-    var southamerica = object.getObjectByName("Sydamerika");
-    southamerica.scale.set(1,1,1);
+     var southamerica = object.getObjectByName("Sydamerika");
+     southamerica.scale.set(1,1,1);
 
-    var africa = object.getObjectByName("Afrika");
-    africa.scale.set(1,1,1);*/
+     var africa = object.getObjectByName("Afrika");
+     africa.scale.set(1,1,1);*/
 
     /****************************************
-            GÖR KARTAN TILL EN SFÄR
+     GÖR KARTAN TILL EN SFÄR
      ***************************************/
 
 
@@ -154,20 +182,23 @@ loader.load('thisistheultimatemap.obj', function (object) {
     object.rotation.x = Math.PI/2 - Math.PI/8;
     object.rotation.z = Math.PI/2 + Math.PI/8;
 
-     scene.add(object);
+    scene.add(object);
 
 } );
 
 
 
 
-  function render()
-  {
-  requestAnimationFrame(render);
-  controls.update();
-  renderer.render(scene, camera);
-  }
-  render();
+function render()
+{
+    requestAnimationFrame(render);
+    controls.update();
+    renderer.render(scene, camera);
+
+    starMesh.rotation.x += 0.0003;
+
+}
+render();
 
 
 
